ca3fd88f769059dbeba54ea6d2537699a105ba39
Prysline <mayhu1993@gmail.com>
2021-11-06 23:48:38 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') // 載入 mongoose
 
 const dataList = require('./restaurant.json')
 
 // Define server related variables
 const port = 3000
+
+// setting mongoose server
+mongoose.connect('mongodb://localhost/restaurant-list') // 設定連線到 mongoDB
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
