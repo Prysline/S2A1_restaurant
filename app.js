92b18a491aa6fc4d3b4ba4ac5d3f1d01eb8d60ca
Prysline <mayhu1993@gmail.com>
2021-11-07 19:57:20 +0800

    feat: add create restaurant data function

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
 
 // Route
 app.get('/', (req, res) => {
@@ -57,6 +59,30 @@ app.get('/search', (req, res) => {
   .catch(error => console.error(error))
 })
 
+app.get('/new', (req, res) => {
+  Restaurant.find()
+  .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+  .then(restaurants => {
+    // 列出資料庫中所有出現過的餐廳類別
+    const category = [...new Set(restaurants.map((item) => item.category))]
+    res.render('new', { category })
+  })
+  .catch(error => console.error(error))
+})
+
+app.post('/new', (req, res) => {
+  let body = req.body
+  Restaurant.find()
+  .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+  .then(restaurants => {
+    body.id = Object.keys(restaurants).length + 1
+    Restaurant.create(body)
+    .then(()=> {
+      res.redirect('/')
+    })
+    .catch(error => console.error(error))
+  })
+  .catch(error => console.error(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
