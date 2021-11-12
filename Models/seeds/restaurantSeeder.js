db3d8000775c2a3f20595c3408e7210785145c15
Prysline <mayhu1993@gmail.com>
2021-11-12 18:00:32 +0800

    refactor: modify mongoose config

--- Models/seeds/restaurantSeeder.js
+++ Models/seeds/restaurantSeeder.js
@@ -1,15 +1,7 @@
-const mongoose = require('mongoose') // 載入 mongoose
+const db = require('../../config/mongoose') // 載入 mongoose
 const Restaurant = require('../database')
 const dataList = require('./restaurant.json')
 
-// setting mongoose server
-mongoose.connect('mongodb://localhost/restaurant-list') // 設定連線到 mongoDB
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
 const methodOverride = require('method-override') // 載入 method-override
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 // Define server related variables
 const port = 3000
-
-// setting mongoose server
-mongoose.connect('mongodb://localhost/restaurant-list') // 設定連線到 mongoDB
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
