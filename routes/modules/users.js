5eb55586297b4d6c3de79c601f5254611387cee2
Prysline <mayhu1993@gmail.com>
2022-01-09 16:09:37 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../Models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -9,6 +11,19 @@ router.post('/login', (req, res) => {
 })
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', { name, email, password, confirmPassword })
+    } else {
+      return User.create({ name, email, password, confirmPassword })
+        .then(() => res.redirect('/users/login'))
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 // 匯出路由模組
