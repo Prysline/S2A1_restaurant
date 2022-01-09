9cf6b496f9ecac04874c7b741f4ad678a74e9815
Prysline <mayhu1993@gmail.com>
2022-01-09 15:40:34 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,11 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', (req, res) => {
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 // 匯出路由模組
 module.exports = router
