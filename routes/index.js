46a6f4f4972c4c14c9172f2f61a6f90a8f0bfd39
Prysline <mayhu1993@gmail.com>
2022-01-09 16:45:35 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const restaurants = require('./modules/restaurants')
+const { authenticator } = require('../middleware/auth')
 
 // 將網址結構符合字串的 request 導向模組
-router.use('/', home)
-router.use('/index', home)
 router.use('/users', users)
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/index', authenticator, home)
+router.use('/', authenticator, home)
 
 // 匯出路由器
 module.exports = router
