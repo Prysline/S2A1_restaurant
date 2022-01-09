44c9c105e9b9cb8832fcadb2028a1fcfebd4f0ea
Prysline <mayhu1993@gmail.com>
2022-01-09 15:27:09 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const users = require('./modules/users')
 const restaurants = require('./modules/restaurants')
 
 // 將網址結構符合字串的 request 導向模組
 router.use('/', home)
 router.use('/index', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
 
 // 匯出路由器
