e88e150d929037aa8d7c43562f85b446c653b9e0
Prysline <mayhu1993@gmail.com>
2022-01-13 17:19:55 +0800

    fix: 修正搜尋關鍵字時會從所有資料庫中查詢的問題

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -18,10 +18,12 @@ router.get('/', (req, res) => {
 })
 
 router.get('/search', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword.toLowerCase()
   const sort = req.query.sort
   const listSort = getSortObj(sort)
   Restaurant.find()
+  Restaurant.find({ userId })
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort(listSort)
     .then(restaurants => {
